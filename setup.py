47ae9684a5b020cc990297309e382a9c72d56002
Ping <xianpingwu@hotmail.com>
2024-04-01 21:34:05 +1100

    update version 0.1.1

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ERA_V2_Architecture",
-    version="0.1.0",
+    version="0.1.1",
     author="Xianping Wu",
     author_email="xianpingwu@hotmail.com",
-    description="The ERA-V2 course delves deep into the realm of cutting-edge network architectures in the field of deep learning. Designed for seasoned practitioners and enthusiasts alike, this course offers a comprehensive exploration of Convolutional Neural Networks (CNNs), Deep Neural Networks (DNNs), and Transformers. Through a combination of theoretical lectures, practical implementations, and hands-on exercises, participants will gain a profound understanding of these advanced architectures, their underlying principles, and their applications across various domains.",
+    description="The ERA-V2 course network architectures",
     long_description=long_description,
     long_description_content_type="text/markdown",    
     url="https://github.com/ping-Mel/ERAV2-Architecture.git",
