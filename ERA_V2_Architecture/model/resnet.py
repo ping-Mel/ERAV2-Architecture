cc256247d75f2db2a993a592503af2c807bb333b
Ping <xianpingwu@hotmail.com>
2024-04-12 23:01:33 +1000

    add classes

--- ERA_V2_Architecture/model/resnet.py
+++ ERA_V2_Architecture/model/resnet.py
@@ -40,7 +40,7 @@ class BasicBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10):  
         super(ResNet, self).__init__()
         self.in_planes = 64
 
@@ -51,6 +51,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # Adaptive pooling to adjust pooling size automatically
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -67,7 +68,8 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        #out = F.avg_pool2d(out, 4)
+        out = self.avgpool(out)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
